f0efdcf18b9d1262ba14e4aba4b7f160a68b0079
George Bodea <bodea.george12@yahoo.com>
2023-05-08 23:58:48 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-dom/client'],
+    }
+  },
 
 server: {
 
